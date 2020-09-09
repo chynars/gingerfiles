7b2d41ef7d9aa7050fe0e34ce968f72f8213bc20
Chyna Stephens <stephenscr@mymail.vcu.edu>
2020-09-08 23:30:46 -0400

    first commit

--- pg 84/aliens.py
+++ pg 84/aliens.py
@@ -1,3 +1,30 @@
 #chyna
 #this is a practice assignment on page 84
 
+alien_color = 'red'
+#green = 5
+# yellow = 10
+# red = 15
+def get_points(killed_color):
+    earned_points = 0 
+
+    if killed_color == 'green':
+        earned_points = 5
+    elif killed_color == 'yellow':
+        earned_points = 10
+    elif killed_color == 'red':
+        earned_points = 15
+    elif killed_color == 'blue':
+        earned_points = 3
+    
+    return earned_points
+
+
+if __name__ == "__main__":
+    pts = get_points('red')
+    print("For red you got: {} points".format(pts))
+
+    assert get_points('red') == 15
+    assert get_points('yellow') == 10
+    assert get_points('green') == 5
+    assert get_points('blue') == 3
